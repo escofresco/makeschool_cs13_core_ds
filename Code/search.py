88835948fb7c6a462ca480166b937e6b71c28331
Jonasz Karwacki <jonaszakr@gmail.com>
2020-01-28 17:06:30 -0800

    complete recur lin search

--- Code/search.py
+++ Code/search.py
@@ -4,7 +4,7 @@ def linear_search(array, item):
     """return the first index of item in array or None if item is not found"""
     # implement linear_search_iterative and linear_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    return linear_search_iterative(array, item)
+    return linear_search_recursive(array, item)
     # return linear_search_recursive(array, item)
 
 
@@ -17,8 +17,11 @@ def linear_search_iterative(array, item):
 
 
 def linear_search_recursive(array, item, index=0):
-    # TODO: implement linear search recursively here
-    pass
+    if index > len(array):
+        return
+    if array[index] == item:
+        return index
+    return linear_search_recursive(array, item, index+1)
     # once implemented, change linear_search to call linear_search_recursive
     # to verify that your recursive implementation passes all tests
 
