33d7b0eed526f615963bda619a845cfbdc1959a9
Jonasz Karwacki <jonaszakr@gmail.com>
2020-02-18 22:03:35 -0800

    fix pop() bug

--- Code/stack.py
+++ Code/stack.py
@@ -21,27 +21,49 @@ class LinkedStack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
-        # TODO: Check if empty
+        return self.list.is_empty()
 
     def length(self):
         """Return the number of items in this stack."""
-        # TODO: Count number of items
+        return self.list.length()
 
     def push(self, item):
         """Insert the given item on the top of this stack.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Push given item
+        self.list.append(item)
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
-        # TODO: Return top item, if any
+        try:
+            return self.list.get_at_index(self.list.size - 1)
+        except:
+            pass
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return top item, if any
+        Running time: """
+
+        if self.list.is_empty():
+            raise ValueError("stack is empty.")
+        elif self.list.length() == 1:
+            self.list.size -= 1
+            tail = self.list.tail
+            self.list.head = self.list.tail = None
+            return tail.data
+        tail = self.list.tail
+        cur = self.list.head
+
+        while cur.next is not tail:
+            cur = cur.next
+        self.list.tail = cur
+        self.list.tail.next = None
+
+        if cur is self.list.head:
+            self.list.head = self.list.tail
+        self.list.size -= 1
+        return tail.data
 
 
 # Implement ArrayStack below, then change the assignment at the bottom
